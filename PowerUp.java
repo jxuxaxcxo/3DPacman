eede3aef0b1f816880b4250847db73ce4ea1d3ae
jxuxaxcxo <joaquin.vargaselio@gmail.com>
2016-02-25 16:18:21 -0500

    Update PowerUp.java
    public methods --> getX(), getY()
    
    boolean isEatenBy( Pacman p)-- > takes a Pacman object as a parameter, and if X and Y coordinates, are the same as the "Power up", returns true;

--- PowerUp.java
+++ PowerUp.java
@@ -1,8 +1,28 @@
 
 public class PowerUp extends Edible{
 
+	private int xPos, yPos;
+	
 	public PowerUp(int x, int y) {
 		super(x, y);
 	}
+	
+	
+	public boolean isEatenBy(Pacman p){
+		if (p.getX()==xPos && p.getY()==yPos)
+		return true;
+		
+		else 
+		return false;
+	}
+	
+	public int getX(){
+		return xPos;
+	}
+	
+	public int getY(){
+		return yPos;
+	}
+	
 
 }
