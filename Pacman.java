77d325d26eabc09e981f6482d6f2dee106790154
exk22 <exk22@scasd.org>
2016-02-25 16:36:45 -0500

    Update Pacman.java

--- Pacman.java
+++ Pacman.java
@@ -1,8 +1,13 @@
 
 public class Pacman extends Movable{
-
+	private int livesLeft;
+	
 	public Pacman(int x, int y) {
 		super(x, y);
+		livesLeft = 3;
 	}
-
+	
+	public boolean stillAlive(){
+		return livesLeft > 0;
+	}
 }
